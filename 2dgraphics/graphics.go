17d2be87fab14c1e77b046e99b4b0f3d952cf93c
Mario Groß <turgeon28@gmail.com>
2020-07-13 21:38:20 +0200

    Add picture and texture loading

---
+++ 2dgraphics/graphics.go
@@ -0,0 +1,142 @@
+package main
+
+//SDL initialisation code with colors ;)
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const winWidth, winHeight int = 800, 600
+
+type texture struct {
+	pixels      []byte
+	w, h, pitch int
+}
+
+type rgba struct {
+	r, g, b byte
+}
+
+type pos struct {
+	x, y float32
+}
+
+func setPixel(x, y int, c rgba, pixels []byte) {
+	index := (y*winWidth + x) * 4
+
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
+
+}
+
+func (tex *texture) draw(p pos, pixels []byte) {
+	for y := 0; y < tex.h; y++ {
+		for x := 0; x < tex.w; x++ {
+			screenY := y + int(p.y)
+			screenX := x + int(p.x)
+			if screenX >= 0 && screenX < winWidth && screenY >= 0 && screenY < winHeight {
+				texIndex := y*tex.pitch + x*4
+				screenIndex := screenY*winWidth*4 + screenX*4
+
+				pixels[screenIndex] = tex.pixels[texIndex]
+				pixels[screenIndex+1] = tex.pixels[texIndex+1]
+				pixels[screenIndex+2] = tex.pixels[texIndex+2]
+				pixels[screenIndex+3] = tex.pixels[texIndex+3]
+			}
+
+		}
+	}
+}
+
+func loadImage(imgpath string) *texture {
+
+	infile, err := os.Open(imgpath)
+	if err != nil {
+		panic(err)
+	}
+	defer infile.Close()
+
+	img, err := png.Decode(infile)
+	if err != nil {
+		panic(err)
+	}
+
+	w := img.Bounds().Max.X
+	h := img.Bounds().Max.Y
+	fmt.Println("w,h", w, h)
+	imgPixels := make([]byte, w*h*4)
+	imgIndex := 0
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			imgPixels[imgIndex] = byte(r / 256)
+			imgIndex++
+			imgPixels[imgIndex] = byte(g / 256)
+			imgIndex++
+			imgPixels[imgIndex] = byte(b / 256)
+			imgIndex++
+			imgPixels[imgIndex] = byte(a / 256)
+			imgIndex++
+		}
+	}
+	return &texture{imgPixels, w, h, w * 4}
+}
+
+func main() {
+
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, winWidth*winHeight*4)
+	imgTex := loadImage("Knight1.png")
+	imgTex.draw(pos{0, 0}, pixels)
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		tex.Update(nil, pixels, winWidth*4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+
+		sdl.Delay(16)
+	}
+
+}
